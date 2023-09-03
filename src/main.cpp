33d162719cd134e778213280b8e87a2322149d02
Jiri Masnik <jmasnik@gmail.com>
2023-09-03 11:25:45 +0200

    Dalsi cudliky, pridane MCP, ktere ale blbne, takze se nepouziva.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,31 @@
 #include "Arduino.h"
 #include "HID-Project.h"
+#include <Adafruit_MCP23X17.h>
 
-#define SWITCH_COUNT 4
+#define SWITCH_COUNT 10
 #define AXIS_COUNT 6
-#define BUTTON_TMR_MAX 200
+#define BUTTON_TMR_MAX 150
+#define SWITCH_TMR_MAX 300
+#define PIN_MCP1_RST 7
+
+#define BUS_INTERNAL 1
+#define BUS_MCP1 2
+
+#define TYPE_SWITCH 1
+#define TYPE_BUTTON 2
+
+#define MCP_GPB0 8
 
 // cudlik
 typedef struct {
-  uint8_t pin;
-  
-  uint8_t button_a;
-  uint8_t button_a_tmr;
-  
-  uint8_t button_b;
-  uint8_t button_b_tmr;
-  
-  uint8_t actual_value;
+  uint8_t pin;              // ktery pin to je na HW
+  uint8_t bus;              // kde ten pin je
+  uint8_t type;             // jaky typ cvakatka to je
+  uint8_t button_a;         // co mackam na gamepadu
+  uint16_t button_a_tmr;
+  uint8_t button_b;         // co mackam na gamepadu
+  uint16_t button_b_tmr;
+  uint8_t actual_value;     // jaky je aktualni stav
 } hwswitch;
 
 // analogova osa
@@ -34,7 +44,6 @@ typedef struct {
 hwswitch switch_list[SWITCH_COUNT];
 hwaxis axis_list[AXIS_COUNT]; 
 
-const int pinLed = LED_BUILTIN;
 const int pinButton = 2;
 
 uint16_t i;
@@ -42,29 +51,65 @@ uint8_t gp_write;
 
 unsigned long ms_serial;
 unsigned long ms_gamepad;
+unsigned long ms_buttons;
+
+Adafruit_MCP23X17 mcp1;
+
+void initButton(uint8_t id, uint8_t bus, uint8_t pin, uint8_t gamepad_button);
 
 void setup() {
+  uint8_t cntr;
 
   ms_serial = 0;
   ms_gamepad = 0;
+  ms_buttons = 0;
 
   Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(50000);
   
-  switch_list[0].pin = 2;
-  switch_list[0].button_a = 1;
-  switch_list[0].button_b = 2;
+  cntr = 0;
 
-  switch_list[1].pin = 3;
-  switch_list[1].button_a = 3;
-  switch_list[1].button_b = 4;
+  switch_list[cntr].bus = BUS_INTERNAL;
+  switch_list[cntr].pin = 2;
+  switch_list[cntr].type = TYPE_SWITCH;
+  switch_list[cntr].button_a = 1;
+  switch_list[cntr].button_b = 2;
+  cntr++;
 
-  switch_list[2].pin = 4;
-  switch_list[2].button_a = 5;
-  switch_list[2].button_b = 6;
+  switch_list[cntr].bus = BUS_INTERNAL;
+  switch_list[cntr].pin = 3;
+  switch_list[cntr].type = TYPE_SWITCH;
+  switch_list[cntr].button_a = 3;
+  switch_list[cntr].button_b = 4;
+  cntr++;
 
-  switch_list[3].pin = 5;
-  switch_list[3].button_a = 7;
-  switch_list[3].button_b = 8;
+  switch_list[cntr].bus = BUS_INTERNAL;
+  switch_list[cntr].pin = 4;
+  switch_list[cntr].type = TYPE_SWITCH;
+  switch_list[cntr].button_a = 5;
+  switch_list[cntr].button_b = 6;
+  cntr++;
+
+  switch_list[cntr].bus = BUS_INTERNAL;
+  switch_list[cntr].pin = 5;
+  switch_list[cntr].type = TYPE_SWITCH;
+  switch_list[cntr].button_a = 7;
+  switch_list[cntr].button_b = 8;
+  cntr++;
+
+  initButton(cntr, BUS_INTERNAL, 6, 9);
+  cntr++;
+  initButton(cntr, BUS_INTERNAL, 7, 10);
+  cntr++;
+  initButton(cntr, BUS_INTERNAL, 8, 11);
+  cntr++;
+  initButton(cntr, BUS_INTERNAL, 9, 12);
+  cntr++;
+  initButton(cntr, BUS_INTERNAL, 10, 13);
+  cntr++;
+  initButton(cntr, BUS_INTERNAL, 11, 14);
+  cntr++;
 
   // plyn
   axis_list[0].pin = A5;
@@ -132,70 +177,177 @@ void setup() {
   axis_list[5].out_low = -128;
   axis_list[5].out_high = 127;
 
-  pinMode(pinLed, OUTPUT);
+  // pin s interni LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 
+  // reset MCP1 neresetuje
+  //pinMode(PIN_MCP1_RST, OUTPUT);
+
+  //digitalWrite(PIN_MCP1_RST, LOW);
+  //delay(200);
+  //digitalWrite(PIN_MCP1_RST, HIGH);
+  //delay(500);
+
+  // mcp init
+  /*
+  if (!mcp1.begin_I2C()) {
+    while(1){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(500);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(500);
+    }
+  }
+  */
+
+  // nastaveni typu pinu
   for(i = 0; i < SWITCH_COUNT; i++){
-    pinMode(switch_list[i].pin, INPUT_PULLUP);
+
+    switch_list[i].button_a_tmr = 0;
+    switch_list[i].button_b_tmr = 0;
+
+    if(switch_list[i].bus == BUS_INTERNAL){
+      pinMode(switch_list[i].pin, INPUT_PULLUP);
+    }
+    
+    if(switch_list[i].bus == BUS_MCP1){
+      //mcp1.pinMode(switch_list[i].pin, INPUT_PULLUP);
+    }
   }
 
+  // aktualni stav spinacu
   for(i = 0; i < SWITCH_COUNT; i++){
-    if(digitalRead(switch_list[i].pin) == HIGH){
-      switch_list[i].actual_value = 1;
-    } else {
-      switch_list[i].actual_value = 0;
+    if(switch_list[i].bus == BUS_INTERNAL){
+      if(digitalRead(switch_list[i].pin) == HIGH){
+        switch_list[i].actual_value = 1;
+      } else {
+        switch_list[i].actual_value = 0;
+      }
     }
+
+/*
+    if(switch_list[i].bus == BUS_MCP1){
+      if(mcp1.digitalRead(switch_list[i].pin) == HIGH){
+        switch_list[i].actual_value = 1;
+      } else {
+        switch_list[i].actual_value = 0;  
+      }
+    }
+    */
+
   }
 
   gp_write = 1;
 
+/*
+  uint8_t val;
+  while(1){
+    val = mcp1.digitalRead(MCP_GPB0);
+    Serial.println(val);
+    delay(250);
+  }
+*/
+
+  // gamepad init
   Gamepad.begin();
+
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
   uint16_t adc_lim;
   int32_t out_range;
+  int pin_val;
+  unsigned long millis_act;
 
   for(i = 0; i < SWITCH_COUNT; i++){
     if(switch_list[i].button_a_tmr == 0 && switch_list[i].button_b_tmr == 0){
-      if(digitalRead(switch_list[i].pin) == HIGH && switch_list[i].actual_value == 0){
-        // zmena na 1
-        switch_list[i].actual_value = 1;
-        switch_list[i].button_a_tmr = BUTTON_TMR_MAX;
+      
+      if(switch_list[i].bus == BUS_INTERNAL){
+        pin_val = digitalRead(switch_list[i].pin);
       }
-      if(digitalRead(switch_list[i].pin) == LOW && switch_list[i].actual_value == 1){
-        // zmena na 0
-        switch_list[i].actual_value = 0;
-        switch_list[i].button_b_tmr = BUTTON_TMR_MAX;
+      if(switch_list[i].bus == BUS_MCP1){
+        //pin_val = mcp1.digitalRead(switch_list[i].pin);
+        //delay(100);
+        pin_val = 1;
       }
+
+      if(switch_list[i].type == TYPE_SWITCH){
+        if(pin_val == HIGH && switch_list[i].actual_value == 0){
+          // zmena na 1
+          switch_list[i].actual_value = 1;
+          switch_list[i].button_a_tmr = SWITCH_TMR_MAX;
+        }
+        if(pin_val == LOW && switch_list[i].actual_value == 1){
+          // zmena na 0
+          switch_list[i].actual_value = 0;
+          switch_list[i].button_b_tmr = SWITCH_TMR_MAX;
+        }
+      }
+
+      if(switch_list[i].type == TYPE_BUTTON){
+        if(pin_val == LOW){
+          // je stisknut
+          switch_list[i].actual_value = 0;
+          switch_list[i].button_a_tmr = BUTTON_TMR_MAX;
+        }
+        if(pin_val == HIGH){
+          // neni stisknut
+          switch_list[i].actual_value = 1;
+        }
+      }
+
     }
   }
 
-  for(i = 0; i < SWITCH_COUNT; i++){
-    
-    if(switch_list[i].button_a_tmr > 0){
-      if(switch_list[i].button_a_tmr == BUTTON_TMR_MAX){
-        Gamepad.press(switch_list[i].button_a);
-        gp_write = 1;
-      }
-      if(switch_list[i].button_a_tmr == 1){
-        Gamepad.release(switch_list[i].button_a);
-        gp_write = 1;
-      }
-      switch_list[i].button_a_tmr--;
-    }
+  // mackani tlacitek na gmaepadu
+  millis_act = millis();
+  if(millis_act != ms_buttons){
+    for(i = 0; i < SWITCH_COUNT; i++){
+      
+      if(switch_list[i].button_a_tmr > 0){
 
-    if(switch_list[i].button_b_tmr > 0){
-      if(switch_list[i].button_b_tmr == BUTTON_TMR_MAX){
-        Gamepad.press(switch_list[i].button_b);
-        gp_write = 1;
+        if(switch_list[i].type == TYPE_SWITCH){
+          if(switch_list[i].button_a_tmr == SWITCH_TMR_MAX){
+            Gamepad.press(switch_list[i].button_a);
+            gp_write = 1;
+          }
+          if(switch_list[i].button_a_tmr == 1){
+            Gamepad.release(switch_list[i].button_a);
+            gp_write = 1;
+          }
+          switch_list[i].button_a_tmr--;
+        }
+
+        if(switch_list[i].type == TYPE_BUTTON){
+          if(switch_list[i].button_a_tmr == BUTTON_TMR_MAX){
+            Gamepad.press(switch_list[i].button_a);
+            gp_write = 1;
+          }
+          if(switch_list[i].button_a_tmr == 1){
+            Gamepad.release(switch_list[i].button_a);
+            gp_write = 1;
+          }
+          switch_list[i].button_a_tmr--;
+        }
       }
-      if(switch_list[i].button_b_tmr == 1){
-        Gamepad.release(switch_list[i].button_b);
-        gp_write = 1;
-      }
-      switch_list[i].button_b_tmr--;
-    }    
-  
+
+      if(switch_list[i].button_b_tmr > 0){
+        if(switch_list[i].type == TYPE_SWITCH){
+          if(switch_list[i].button_b_tmr == BUTTON_TMR_MAX){
+            Gamepad.press(switch_list[i].button_b);
+            gp_write = 1;
+          }
+          if(switch_list[i].button_b_tmr == 1){
+            Gamepad.release(switch_list[i].button_b);
+            gp_write = 1;
+          }
+          switch_list[i].button_b_tmr--;
+        }
+      }    
+    }
+    ms_buttons = millis_act;
   }
 
   // Nacteni analogovych vstupu
@@ -260,4 +412,20 @@ void loop() {
     ms_serial = millis();
   }
 
+}
+
+/**
+ * Definice cudliku
+ */
+void initButton(uint8_t id, uint8_t bus, uint8_t pin, uint8_t gamepad_button){
+  if(id >= SWITCH_COUNT) return;
+
+  switch_list[id].bus = bus;
+  switch_list[id].pin = pin;
+  switch_list[id].type = TYPE_BUTTON;
+  switch_list[id].button_a = gamepad_button;
+  switch_list[id].button_a_tmr = 0;
+  switch_list[id].button_b = 0;
+  switch_list[id].button_b_tmr = 0;
+  switch_list[id].actual_value = 1;  
 }
